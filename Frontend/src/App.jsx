4b187ea1c983564e2a9e23a43682567cff14f5b9
soyakkumarkp <soyalkumarkp@gmail.com>
2023-11-13 11:46:26 +0530

    initail setup of react is done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,7 @@
-export default function App() {
+import React from "react";
+
+const App = () => {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+};
+
+export default App;
